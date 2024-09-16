2a70a5412a94f98c49b9d7e792aaeaaad86a6942
Mitch Wodach <wodachm@gmail.com>
2024-09-16 16:47:49 -0500

    update  lval stuff

--- inter_edit.c
+++ inter_edit.c
@@ -53,16 +53,18 @@ return lval_err(LERR_BAD_OP);
 }
 
 
-float eval(mpc_ast_t* t)
+lval eval(mpc_ast_t* t)
 {
 	if(strstr(t->tag, "number"))
 	{	
-		return atof(t->contents);
+		errno = 0;
+		float x = strtof(t->contents,NULL);
+		return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
 	}	
 
 	char* op = t->children[1]->contents;
 	
-	float x = eval(t->children[2]);
+	lval x = eval(t->children[2]);
 
 	int i = 3;
 	while (strstr(t->children[i]->tag,"expr"))
@@ -115,17 +117,10 @@ int main(int argc, char** argv)
 		
 		if(mpc_parse("<stdin>",input,Lisp,&r))
 		{
-			float result = eval(r.output);
-			printf("%f\n",result);
+			lval result = eval(r.output);
+			lval_println(result);
 			mpc_ast_delete(r.output);
 		}
-		
-		//else
-		//{
-		//	mpc_err_print(r.error);
-		//	mpc_err_delete(r.error);
-		//}
-		
 		
 		
 		free(input);
